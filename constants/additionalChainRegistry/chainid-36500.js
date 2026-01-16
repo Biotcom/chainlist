d2f5dae2962da9cfdd65af9249bde6a061e080de
Biot <48119728+Biotcom@users.noreply.github.com>
2026-01-16 16:14:06 +0800

    Create chainid-36500.js

---
+++ constants/additionalChainRegistry/chainid-36500.js
@@ -0,0 +1,25 @@
+{
+  "name": "VoidChain Testnet",
+  "chain": "VoidChain",
+  "rpc": [
+    "https://api-backend.voidchain.net/api",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "TC",
+    "symbol": "TC",
+    "decimals": 9
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://voidchain.net/",
+  "shortName": "TC",
+  "chainId": 36500,
+  "networkId": 6,
+  "icon": "TC",
+  "explorers": [{
+    "name": "VoidChain",
+    "url": "https://explorer.voidchain.net/#/",
+    "icon": "VoidChain",
+    "standard": ""
+  }]
+}
