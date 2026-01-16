ae495a933539f6e45cd77db5fd34bad76a17186d
Biot <48119728+Biotcom@users.noreply.github.com>
2026-01-16 16:08:17 +0800

    Create chainid-36500.js
    VoidChain is not just another blockchain — it's the infrastructure for the next billion users with infinite scalability and unmatched security.

---
+++ constants/additionalChainRegistry/chainid-36500.js
@@ -0,0 +1,25 @@
+export const data = {
+    "name": "VoidChain Testnet",
+    "chain": "VoidChain",
+    "icon": "",
+    "rpc": [
+      "http://120.24.21.132:8080"
+    ],
+    "features": [{ "name": "EIP155" }],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "VoidChain",
+      "symbol": "TC",
+      "decimals": 9
+    },
+    "infoURL": "",
+    "shortName": "ethw",
+    "chainId": 36500,
+    "networkId": 6,
+    "explorers": [
+      {
+        "name": "",
+        "url": ""
+      }
+    ]
+  }
